25aa831c2ef1dd9cc3c70b1377889ea53a0ac5a8
imatheus-lucas <matheeuslucas13@gmail.com>
2021-01-07 15:12:08 -0300

    modified: port mailer

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,3 +1,5 @@
+import transport from "../../config/mail"
+
 export default {
     async store(req, res){
         const { name, email, password } = req.body
@@ -7,6 +9,12 @@ export default {
             name,email, password
         }
 
+        await  transport.sendMail({
+            from:'Marcos <queue@queue@queuetest.com.br>',
+            to: `${name} <${email}>`,
+            subject:'Cadastro de usuario',
+            html:`Olá ${name}, bem vindo ao sistema de filas`
+        })
         return res.json(user)
     }
 }
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,11 @@
+import nodemailer from 'nodemailer'
+
 const transport = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
-    port: process.env.PORT,
+    port: process.env.MAIL_PORT,
     auth: {
-      user: process.env.USER,
-      pass: process.env.PASS
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS
     }
   });
 
