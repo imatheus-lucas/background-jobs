08795a8c0e552a4e6fc15b841e8047014064a798
imatheus-lucas <matheeuslucas13@gmail.com>
2021-01-07 14:53:16 -0300

    add userController

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import userController from './app/controllers/userController';
 
 const app = express();
 
+
+app.post('/users', userController.store)
 app.listen(3333 , () => {
     console.log('server started')
 })
